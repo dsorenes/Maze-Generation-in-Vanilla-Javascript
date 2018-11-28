917caa6a634e92ed227032f4cbb372b86bd99f1c
Daniel Sørenes <Danielsorenes@hotmail.no>
2018-11-28 03:46:35 +0100

    WIP maze-solver, minor refactoring

---
+++ src/maze-solver.js
@@ -0,0 +1,19 @@
+
+// for (let node of nodes) {
+//     for (let n of node) {
+//         console.log(n);
+//     }
+// }
+
+
+console.table(nodes);
+
+const start = nodes[0][0];
+const end = nodes[rows - 1][columns - 1];
+
+console.log(start, end);
+
+
+function solve() {
+    
+}
