d77ad54e4716f1c69c7c58a0b0ff01f919b27a30
Daniel Sørenes <Danielsorenes@hotmail.no>
2018-11-28 02:17:56 +0100

    few tweaks and refactors

--- main.js
+++ main.js
@@ -4,94 +4,124 @@ const ctx = canvas.getContext('2d');
 const width = 600;
 const height = 600;
 
-let nodesize = 20;
-let rows = width / nodesize;
-let columns = height / nodesize;
+const nodesize = 40;
+const rows = width / nodesize;
+const columns = height / nodesize;
 
 let stack = [];
 
 function getRandomInt(max) {
+
     return Math.floor(Math.random() * Math.floor(max));
+
 }
 
 let nodes = new Array(rows);
-//creates the grid and nodes
-for (let x = 0; x < rows; x++) {
-    nodes[x] = new Array(columns);
-    for (let y = 0; y < columns; y++) {
-        let node = new Node(x * nodesize, y * nodesize, nodesize);
-        nodes[x][y] = node;
 
-        //coordinate text
-        // ctx.fillText(`${node.x}, ${node.y}`, x * nodesize, y * nodesize + 100 / 10);
+//creates the grid and the nodes within it
+function createGrid() {
+
+    for (let x = 0; x < rows; x++) {
+
+        nodes[x] = new Array(columns);
+
+        for (let y = 0; y < columns; y++) {
+
+            let node = new Node(x * nodesize, y * nodesize, nodesize);
+            nodes[x][y] = node;
+
+            //coordinate text
+            // ctx.fillText(`${node.x}, ${node.y}`, x * nodesize, y * nodesize + 100 / 10);
+        }
     }
 }
 
+
 function getNeighbors (node) {
+
     let neighbors = [];
     let startNode = node;
 
     for (let node of nodes) {
+
         for (let n of node) {
+
             if (Math.abs(n.x - startNode.x) === nodesize && Math.abs(n.y - startNode.y) === nodesize) {
+
             } else if (Math.abs(n.x - startNode.x) <= nodesize && Math.abs(n.y - startNode.y) <= nodesize) {
+
                 if (n.visited === false) {
+
                     neighbors.push(n);
 
                     // console.log(`${n.x}, ${n.y} is neighbor of ${startNode.x}, ${startNode.y}`);
                 }
-
             }
         }
-        
     }
 
     return neighbors;
+
 }
 
 function chooseNeighbor (node) {
+
     let neighbors = getNeighbors(node);
     let next;
     
     if (neighbors.length > 0) {
+
         next = neighbors[getRandomInt(neighbors.length)];
+
     } else {
+
         return undefined;
+
     }
 
     return next;
+    
 }
-
-
 
 function removeWalls(current, next) {
 
     if (next.x > current.x) {
+
         next.walls[3] = false;
         current.walls[1] = false;
+
     }
 
     if (next.x < current.x) {
+
         next.walls[1] = false;
         current.walls[3] = false;
+
     }
 
     if (next.y > current.y) {
+
         next.walls[0] = false;
         current.walls[2] = false;
+
     }
 
     if (next.y < current.y) {
+
         next.walls[2] = false;
         current.walls[0] = false;
+
     }
 
 }
 
+
+createGrid();
 let current = nodes[0][0];
 current.visited = true;
-ctx.fillStyle = 'grey';
+ctx.fillStyle = 'black';
 ctx.fillRect(0, 0, width, height);
+
 
 function generate () {
     current.drawWalls();
@@ -100,11 +130,15 @@ function generate () {
     ctx.fillRect(current.x, current.y, nodesize, nodesize);
     
     if (!stack.includes(current)) {
+
         stack.push(current);
+
     }
 
     let next = chooseNeighbor(current);
+
     if (next) {
+
         removeWalls(current, next);
         ctx.fillStyle = 'white';
         ctx.fillRect(current.x, current.y, nodesize, nodesize);
@@ -113,26 +147,25 @@ function generate () {
         current.visited = true;
 
         ctx.fillStyle = 'black';
-        ctx.fillRect(next.x, next.y, nodesize, nodesize); 
+        ctx.fillRect(next.x, next.y, nodesize, nodesize);
 
-    }
-
-    if (next === nodes[rows - 1]) {
-        console.log('far right');
-        next.walls[1] = true;
     }
 
     if (!next) {
+
         stack.pop();
         current = stack[stack.length - 1];
         next = chooseNeighbor(current);
 
-        ctx.fillStyle = 'black';
+        ctx.fillStyle = 'lightgrey';
         ctx.fillRect(current.x, current.y, nodesize, nodesize);
+
     }
+
+    ctx.strokeRect(0, 0, width, height);
+    ctx.fillStyle = 'white';
+    ctx.fillRect(nodes[rows - 1][columns - 1].x, nodes[rows - 1][columns - 1].y, nodesize, nodesize);
 
 }
 
 let generation = setInterval(generate, 10);
-
-
